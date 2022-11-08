cfc6674edcaab7069604c9e20ae8a4b987f75803
cjtosh <ctosh@cs.ucsd.edu>
2022-11-08 11:28:12 -0500

    rna seq

--- src/rna_seq.py
+++ src/rna_seq.py
@@ -9,6 +9,8 @@ from general_gmm import GeneralGMM
 from balls_kdes import ProbabilityBall
 from cmodels import fit_owl
 from sklearn.metrics import adjusted_rand_score
+from scipy.special import xlogy
+
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='Processing arguments')
@@ -42,14 +44,17 @@ if __name__ == "__main__":
         l1_ball = ProbabilityBall(dist_type='l1', n=X_pca.shape[0], r=eps)
         gmm_tv = fit_owl(gmm_tv, l1_ball, kde=None, repeats=5, admmsteps=2000, verbose=False)
         mask = (gmm_tv.w >= 1.0)
-        results.append({"K":k, 
-                        "epsilon":eps, 
-                        "Weighted Log-likelihood":np.dot(gmm_tv.w, gmm_tv.log_likelihood_vector()),
+        wll = np.dot(gmm_tv.w, gmm_tv.log_likelihood_vector())
+        kl = np.nansum(xlogy(gmm_tv.w , gmm_tv.w)) - wll
+        results.append({"K": k, 
+                        "epsilon": eps, 
+                        "Weighted Log-likelihood": wll,
+                        "KL divergence": kl,
                         "Adjusted Rand Index": adjusted_rand_score(groups_no_b, gmm_tv.z),
                         "Adjusted Rand Index (with batches)":  adjusted_rand_score(groups, gmm_tv.z),
                         "Adjusted Rand Index (subset)":  adjusted_rand_score(groups_no_b[mask], gmm_tv.z[mask]),
                         "Adjusted Rand Index (subset, with batches)":  adjusted_rand_score(groups[mask], gmm_tv.z[mask])
-                        }) 
+                        })
             
     with open(fname, 'wb') as io:
         pickle.dump(results, io)
