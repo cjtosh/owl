9fe18c799fb968813973aefde20f2831cc1b0f4c
cjtosh <ctosh@cs.ucsd.edu>
2023-01-06 10:39:20 -0500

    dim

---
+++ scripts/gaussian_simulation_high.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+source /home/toshc/anaconda3/bin/activate
+conda activate py39
+
+seed=$(($LSB_JOBINDEX + 100))
+
+python src/gaussian_simulations.py --seed $seed --corr_type 'max' --n 200 --scale 10.0 --dim 100
+python src/gaussian_simulations.py --seed $seed --corr_type 'rand' --n 200 --scale 10.0 --dim 100
