bddd4505d9756ec28c1ad42f6b9b13f025578561
cjtosh <ctosh@cs.ucsd.edu>
2023-02-15 14:59:59 -0800

    reproduce

--- scripts/gmm_simulation.sh
+++ scripts/gmm_simulation.sh
@@ -5,5 +5,5 @@ conda activate owl
 
 seed=$(($LSB_JOBINDEX + 100))
 
-python simulations/gmm_simulation.py --seed $seed --corr_type 'max' --n 200 --stdv 0.25 --p 2
-python simulations/gmm_simulation.py --seed $seed --corr_type 'rand' --n 200 --stdv 0.25 --p 2
+python simulations/gmm_simulation.py --seed $seed --corr_type 'max'
+python simulations/gmm_simulation.py --seed $seed --corr_type 'rand'
